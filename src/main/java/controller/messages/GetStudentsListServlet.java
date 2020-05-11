919f5197d12d39ce8e14e6fcdd47519b00621466
Karol Tomczak <karoltomek@tlen.pl>
2020-05-11 06:51:23 +0200

    resolver error during choosing the group

--- src/main/java/controller/messages/GetStudentsListServlet.java
+++ src/main/java/controller/messages/GetStudentsListServlet.java
@@ -1,9 +1,11 @@
 package controller.messages;
 
-import model.Membership;
+import model.SQLSessionFactory;
 import model.user.User;
 import model.user.UserDAO;
 import model.user.UserDAOImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,19 +16,32 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet(name = "GetStudentListServlet", value = "/getstudentlist")
 public class GetStudentsListServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(SQLSessionFactory.class);
     List<User> userList = new ArrayList<>();
     UserDAO user = new UserDAOImpl();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        userList = user.findAllByMembership(Membership.STUDENT);
-        req.setAttribute("studentsList", userList);
-        RequestDispatcher dispatcher = getServletContext()
-                .getRequestDispatcher("/teacher/bar/teacherBarSendMessage.jsp");
-        dispatcher.forward(req, resp);
+        Optional<Object> userIdValue = Optional.ofNullable(req.getSession().getAttribute("userId").toString());
+        Optional<Object> groupIdValue = Optional.ofNullable(req.getParameter("groupId"));
+
+        if (userIdValue.isPresent() && groupIdValue.isPresent()) {
+
+            String groupId = groupIdValue.get().toString();
+            userList = user.findAllByGroup(groupId);
+            req.setAttribute("studentsList", userList);
+
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/teacher/bar/teacherBarSendMessage.jsp");
+            dispatcher.forward(req, resp);
+        } else {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            logger.info("No userId, subject and student lists not created");
+        }
     }
 }
