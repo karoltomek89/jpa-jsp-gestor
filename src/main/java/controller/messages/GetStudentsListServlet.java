25f2ca18b17c8b08c096229bab423ba879f06d3d
Karol Tomczak <karoltomek@tlen.pl>
2020-05-11 06:53:06 +0200

    remove unused code

--- src/main/java/controller/messages/GetStudentsListServlet.java
+++ src/main/java/controller/messages/GetStudentsListServlet.java
@@ -1,8 +1,11 @@
 package controller.messages;
 
+import model.SQLSessionFactory;
 import model.user.User;
 import model.user.UserDAO;
 import model.user.UserDAOImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,17 +20,12 @@ import java.util.Optional;
 
 @WebServlet(name = "GetStudentListServlet", value = "/getstudentlist")
 public class GetStudentsListServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(SQLSessionFactory.class);
     List<User> userList = new ArrayList<>();
     UserDAO user = new UserDAOImpl();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-//        userList = user.findAllByMembershipType(MembershipType.STUDENT);
-//        req.setAttribute("studentsList", userList);
-//        RequestDispatcher dispatcher = getServletContext()
-//                .getRequestDispatcher("/teacher/bar/teacherBarSendMessage.jsp");
-//        dispatcher.forward(req, resp);
 
         Optional<Object> userIdValue = Optional.ofNullable(req.getSession().getAttribute("userId").toString());
         Optional<Object> groupIdValue = Optional.ofNullable(req.getParameter("groupId"));
@@ -43,10 +41,7 @@ public class GetStudentsListServlet extends HttpServlet {
         } else {
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
             dispatcher.forward(req, resp);
-            //logger.info("No userId, subject and student lists not created");
+            logger.info("No userId, subject and student lists not created");
         }
-
-
     }
-
 }
